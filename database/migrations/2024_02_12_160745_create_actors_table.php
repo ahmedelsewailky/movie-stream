89635db2fb2e2a81d3716e28751ab91dded67926
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2024-02-12 18:41:36 +0200

    Correction of a writing error at nullable image

--- database/migrations/2024_02_12_160745_create_actors_table.php
+++ database/migrations/2024_02_12_160745_create_actors_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->unsignedSmallInteger('country')->nullable();
-            $table->string('image')->nullable;
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
