2761fc1718c512d21e799af5ae13ad102021baab
ahmedelsewailky <ahmede.lsewailky@gmail.com>
2024-02-13 14:56:11 +0200

    Confirmation deletation modal

---
+++ resources/views/dashboard/categories/confirm.modal.blade.php
@@ -0,0 +1,20 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade confirm-modal" id="confirmDelete">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <i class='bx bx-error'></i>
+                <p>انت الآن علي وشك حذف هذا السجل، <strong>هل أنت متأكد من هذا الإجراء؟</strong></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">إلغاء</button>
+                <button type="button" class="btn btn-primary">نعم متأكد</button>
+            </div>
+        </div>
+    </div>
+</div>
