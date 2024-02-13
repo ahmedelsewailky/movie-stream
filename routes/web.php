36f485499b401a9ca257c145ae498d76762c77f1
ahmedelsewailky <ahmede.lsewailky@gmail.com>
2024-02-13 11:37:22 +0200

    Add dashboard routes group

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([
+    'prefix' => 'panel',
+    'as' => 'panel.'
+], function() {
+
+    Route::get('/', DashboardController::class);
+
+});
