bb9272131492050452071d4daa49d67296de4807
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2024-03-17 22:12:45 +0200

    Session 17-3-2024

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,44 +1,71 @@
-{{-- Extend master auth layout --}}
-@extends('layouts.auth')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ app()->getLocale() == 'en' ? 'ltr' : 'rtl' }}">
 
-{{-- Page content --}}
-@section('content')
-    <div class="auth-form absolute-position">
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <div class="mb-3">
-                <label for="login" class="form-label">البريد الإلكتروني</label>
-                <input id="login" type="text" class="form-control @error('login') is-invalid @enderror" name="login"
-                    value="{{ old('login') }}" autofocus>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                @error('login')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-            <div class="mb-3">
-                <label for="password" class="form-label text-md-end">كلمة المرور</label>
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@100..900&display=swap" rel="stylesheet">
 
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+    <!-- Bootstrap Css File -->
+    <link rel="stylesheet" href="{{ asset('assets/libs/bootstrap/css/bootstrap.rtl.min.css') }}">
 
-            <div class="mb-3">
-                <label class="form-check-label" for="remember">
-                    <input class="form-check-input me-1" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                    تذكرني علي هذا الجهاز
-                </label>
-            </div>
+    <!-- Css File -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
 
-            <div class="text-center">
-                <button type="submit" class="btn btn-dark">تسجيل الدخول</button>
-            </div>
-        </form>
+<body>
+
+    <div class="body-wrapper">
+        <div class="auth-login-form">
+            <a href="{{ route('website') }}">
+                <img src="{{ asset('logo.png') }}" alt="">
+            </a>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="mb-3">
+                    <label for="login" class="form-label">البريد الإلكتروني</label>
+                    <input id="login" type="text" class="form-control @error('login') is-invalid @enderror" name="login"
+                        value="{{ old('login') }}" autofocus>
+
+                    @error('login')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label text-md-end">كلمة المرور</label>
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-check-label" for="remember">
+                        <input class="form-check-input me-1" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        تذكرني علي هذا الجهاز
+                    </label>
+                </div>
+
+                <div class="text-center">
+                    <button type="submit" class="btn btn-dark">تسجيل الدخول</button>
+                </div>
+            </form>
+        </div>
     </div>
-@endsection
+
+    <!-- Bootstrap Bundle Js File -->
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+
+</html>
