98e6502a37763fd74122fd33e3523a75ebf357be
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2024-03-20 19:14:31 +0200

    Session 20-3-2024

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,19 +37,6 @@
                         <a href="">{{ env('APP_NAME') }}</a>
                     </div>
 
-                    <div class="sidebar-author">
-                        <div class="d-flex">
-                            <div class="flex-shrink-0">
-                                <img src="https://via.placeholder.com/45" width="45" height="45"
-                                    class="rounded-circle me-3" alt="{{ auth()->user()->name }}">
-                            </div>
-                            <div class="flex-grow-1">
-                                <a href="">{{ auth()->user()->name }}</a>
-                                <p>{{ auth()->user()->email }}</p>
-                            </div>
-                        </div>
-                    </div>
-
                     <div class="sidebar-menu">
                         <ul class="nav flex-column">
                             <li class="nav-item">
@@ -106,82 +93,111 @@
             </aside>
 
             <main class="main-content">
-                <div class="page-header">
-                    <div class="d-flex">
-                        <div class="flex-shrink-0 me-2">
-                            <i class="bx bx-sm bx-home"></i>
-                        </div>
-                        <div class="flex-grow-1">
-                            <div class="page-breadcrumb">
-                                <h6>الرئيسية</h6>
-                                <nav aria-label="breadcrumb">
-                                    <ol class="breadcrumb">
-                                        <li class="breadcrumb-item active" aria-current="page">الصفحة الرئيسية</li>
-                                    </ol>
-                                </nav>
+                <div class="container">
+                    <nav class="navbar navbar-expand-lg">
+                        <div class="container-fluid">
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <div class="page-breadcrumb">
+                                    <h6>الرئيسية</h6>
+                                    <nav aria-label="breadcrumb">
+                                        <ol class="breadcrumb mb-0">
+                                            <li class="breadcrumb-item mt-1 active" aria-current="page">الصفحة الرئيسية
+                                            </li>
+                                        </ol>
+                                    </nav>
+                                </div>
+                                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            <img src="https://via.placeholder.com/32" width="32" height="32"
+                                                class="rounded-circle me-2" alt="{{ auth()->user()->name }}">
+                                            {{ auth()->user()->name }}
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                            <li>
+                                                <hr class="dropdown-divider">
+                                            </li>
+                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                    </div>
-                    <button class="btn btn-sm btn-success">
-                        <i class="bx bx-download"></i>
-                        عرض التقرير
-                    </button>
-                </div>
+                    </nav>
 
-                <div class="row my-3">
-                    <div class="col-lg-5">
-                        <div class="card static-card visits-chart-card">
-                            <div class="card-body p-0">
-                                <div class="static-card-title">
-                                    <i class="bx bx-bar-chart"></i>
-                                    إجمالي الزيارات
+                    <div class="row my-3">
+                        <div class="col-lg-4">
+                            <div class="card static-card visits-chart-card">
+                                <div class="card-body p-0">
+                                    <div class="static-card-title">
+                                        <i class="bx bx-bar-chart"></i>
+                                        إجمالي الزيارات
+                                    </div>
+                                    <h5>{{ number_format(8509850) }}</h5>
+                                    <p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد
+                                        النص العربى</p>
+                                    <div id="visits-chart"></div>
                                 </div>
-                                <h5>{{ number_format(8509850) }}</h5>
-                                <p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد
-                                    النص العربى</p>
-                                <div id="visits-chart"></div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-5">
-                        <div class="card static-card posts-chart-card">
-                            <div class="card-body p-0">
-                                <div class="static-card-title">
-                                    <i class="bx bx-bar-chart"></i>
-                                    إجمالي المنشورات
-                                </div>
-                                <div class="d-flex">
-                                    <div class="flex-grow-1">
-                                        <h5>{{ number_format(4500) }}</h5>
-                                        <p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من
-                                            مولد
-                                            النص العربى</p>
-                                    </div>
-                                    <div class="flex-shrink-0">
-                                        <div id="posts-chart"></div>
-                                    </div>
-                                </div>
-                                <div class="row g-0">
-                                    <div class="col">
-                                        <div class="count-item">
-                                            <span>فيلم</span>
-                                            <strong>315</strong>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="count-item">
-                                            <span>مسلسل</span>
-                                            <strong>45</strong>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="count-item">
-                                            <span>برنامج</span>
-                                            <strong>25</strong>
-                                        </div>
-                                    </div>
-                                </div>
 
+                        <div class="col-lg-4">
+                            <div class="card static-card posts-chart-card">
+                                <div class="card-body p-0">
+                                    <div class="static-card-title">
+                                        <i class="bx bx-bar-chart"></i>
+                                        إجمالي المنشورات
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="flex-grow-1">
+                                            <h5>{{ number_format(4500) }}</h5>
+                                            <p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص
+                                                من
+                                                مولد
+                                                النص العربى</p>
+                                        </div>
+                                        <div class="flex-shrink-0">
+                                            <div id="posts-chart"></div>
+                                        </div>
+                                    </div>
+                                    <div class="row g-0">
+                                        <div class="col">
+                                            <div class="count-item">
+                                                <span>فيلم</span>
+                                                <strong>315</strong>
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div class="count-item">
+                                                <span>مسلسل</span>
+                                                <strong>45</strong>
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div class="count-item">
+                                                <span>برنامج</span>
+                                                <strong>25</strong>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4">
+                            <div class="card static-card downloads-chart-card">
+                                <div class="card-body p-0">
+                                    <div class="static-card-title">
+                                        <i class="bx bx-bar-chart"></i>
+                                        مرات التحميل
+                                    </div>
+                                    <h5>{{ number_format(18765) }}</h5>
+                                    <div id="downloads-chart"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -231,6 +247,10 @@
             dataLabels: {
                 enabled: false
             },
+            colors: ['#009688', '#4caf50', '#cddc39'],
+            stroke: {
+                colors: ['#322e49']
+            },
             responsive: [{
                 breakpoint: 480,
                 options: {
@@ -243,8 +263,83 @@
                 }
             }]
         };
-
         var chart = new ApexCharts(document.querySelector("#posts-chart"), options);
+        chart.render();
+
+        var options = {
+            series: [{
+                data: [{
+                        x: '2008',
+                        y: [2800, 4500]
+                    },
+                    {
+                        x: '2009',
+                        y: [3200, 4100]
+                    },
+                    {
+                        x: '2010',
+                        y: [2950, 7800]
+                    },
+                    {
+                        x: '2011',
+                        y: [3000, 4600]
+                    },
+                    {
+                        x: '2012',
+                        y: [3500, 4100]
+                    },
+                    {
+                        x: '2013',
+                        y: [4500, 6500]
+                    },
+                    {
+                        x: '2014',
+                        y: [4100, 5600]
+                    },
+                    {
+                        x: '2015',
+                        y: [7600, 6400]
+                    },
+                    {
+                        x: '2016',
+                        y: [4100, 5600]
+                    }
+                ]
+            }],
+            chart: {
+                height: 150,
+                type: 'rangeBar',
+                zoom: {
+                    enabled: false
+                },
+                sparkline: {
+                    enabled: true
+                },
+            },
+            plotOptions: {
+                bar: {
+                    isDumbbell: true,
+                    columnWidth: 2,
+                    dumbbellColors: [
+                        ['#CDDC39', '#009688']
+                    ]
+                }
+            },
+            legend: {
+                show: false,
+            },
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    type: 'vertical',
+                    gradientToColors: ['#CDDC39', '#009688'],
+                    inverseColors: true,
+                    stops: [0, 100]
+                }
+            },
+        };
+
+        var chart = new ApexCharts(document.querySelector("#downloads-chart"), options);
         chart.render();
     </script>
 </body>
