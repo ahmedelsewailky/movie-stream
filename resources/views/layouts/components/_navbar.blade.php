93e6436aafd2d342c787cb44d8a9ee9c7afc799e
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2024-04-12 00:37:17 +0200

    Session 12-4-2024

--- resources/views/layouts/components/_navbar.blade.php
+++ resources/views/layouts/components/_navbar.blade.php
@@ -3,10 +3,12 @@
         <a class="navbar-brand" href="{{ route('website') }}">
             <img src="{{ asset('logo.png') }}" alt="">
         </a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span class="bx bx-menu"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
@@ -39,56 +41,6 @@
             @else
                 <a href="{{ route('dashboard') }}" class="btn btn-success text-white">لوحة التحكم</a>
             @endguest
-        </div>
-    </div>
-</nav>
-
-<nav class="mobile-navbar">
-    <ul class="nav">
-        <li class="nav-item">
-            <a href="{{ route('dashboard') }}" class="nav-link">
-                @guest
-                    <i class="bx bx-log-in"></i>
-                @else
-                    <img src="https://via.placeholder.com/32" alt="{{ auth()->user()->name }}">
-                @endguest
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ route('website') }}" class="nav-link">
-                <img src="{{ asset('logo.png') }}" alt="{{ env('APP_NAME') }}">
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="javascript:void(0)" class="nav-link" data-bs-toggle="offcanvas" data-bs-target="#mobileNavMenu">
-                <i class="bx bx-menu"></i>
-            </a>
-        </li>
-    </ul>
-
-    <div class="offcanvas offcanvas-end" id="mobileNavMenu">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title">Offcanvas</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                @foreach (\App\Models\Category::whereNull('parent_id')->get() as $parent)
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="javascript:void(0)" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            {{ $parent->name }}
-                        </a>
-                        <ul class="dropdown-menu">
-                            @foreach (\App\Models\Category::whereParentId($parent->id)->get() as $category)
-                                <li><a class="dropdown-item"
-                                        href="{{ route('web.category', $category->slug) }}">{{ $category->name }}</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </li>
-                @endforeach
-            </ul>
         </div>
     </div>
 </nav>
